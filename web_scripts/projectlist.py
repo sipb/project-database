4a11da92ee77378184b03fcf23109a15e7f606b8
markchil <mark.chilenski@gmail.com>
2022-03-15 20:36:59 -0400

    Try it with extra newline.

--- web_scripts/projectlist.py
+++ web_scripts/projectlist.py
@@ -11,12 +11,11 @@ def format_project_list(project_list):
         loader=jinja2.FileSystemLoader("templates"),
         autoescape=True
     )
-    print('Content-type: text/html\n')
-    print(
-        jenv.get_template('projectlist.html').render(
-            project_list=project_list
-        ).encode('utf-8')
-    )
+    result = ''
+    result += 'Content-type: text/html\n\n'
+    result += jenv.get_template('projectlist.html').render(
+        project_list=project_list
+    ).encode('utf-8')
 
 
 def obfuscate_email(email):
@@ -43,7 +42,8 @@ def get_project_table():
 
 def main():
     project_list = get_project_table()
-    format_project_list(project_list)
+    page = format_project_list(project_list)
+    print(page)
 
 
 if __name__ == '__main__':
