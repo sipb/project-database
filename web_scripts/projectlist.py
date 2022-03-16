f0392981ca72c3347462cdc7d71e7d7bf4a290bd
markchil <mark.chilenski@gmail.com>
2022-03-15 20:13:05 -0400

    Add role descriptions.

--- web_scripts/projectlist.py
+++ web_scripts/projectlist.py
@@ -23,16 +23,22 @@ def obfuscate_email(email):
     return email.replace('@', ' [at] ').replace('.', ' [dot] ')
 
 
-def main():
+def get_project_table():
     project_list = db.list_dict_convert(db.get_all_projects())
     for project in project_list:
+        project_id = project['project_id']
         project['links'] = [
-            link['link'] for link in db.get_links(project['project_id'])
+            link['link'] for link in db.get_links(project_id)
         ]
         project['comm_channels'] = [
             obfuscate_email(channel['commchannel'])
-            for channel in db.get_comm(project['project_id'])
+            for channel in db.get_comm(project_id)
         ]
+        project['roles'] = db.get_roles(project_id)
+
+
+def main():
+    project_list = get_project_table()
     render_project_list(project_list)
 
 
