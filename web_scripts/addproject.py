8590b657394307b9af334bf2e1e58c859accddad
markchil <mark.chilenski@gmail.com>
2022-03-16 20:10:40 -0400

    Nonworking pass at dynamic roles.

---
+++ web_scripts/addproject.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import jinja2
+
+
+def format_add_project():
+    """Format the add project interface.
+
+    Returns
+    -------
+    result : str
+        The HTML to display.
+    """
+    jenv = jinja2.Environment(
+        loader=jinja2.FileSystemLoader('templates'),
+        autoescape=True
+    )
+    result = ''
+    result += 'Content-type: text/html\n\n'
+    result += jenv.get_template('addproject.html').render(
+        user='markchil',
+        can_add=True,
+        help_address='dev [slash] null'
+    )#.encode('utf-8')
+    return result
+
+
+def main():
+    """Display the add project interface.
+    """
+    page = format_add_project()
+    print(page)
+
+
+if __name__ == '__main__':
+    main()
