69830f40a19d3cca731537f7cc048000b1cd642f
markchil <mark.chilenski@gmail.com>
2022-03-16 21:21:03 -0400

    Actually uncomment roles.

--- web_scripts/performaddproject.py
+++ web_scripts/performaddproject.py
@@ -28,7 +28,7 @@ def render_result_page(arguments):
         contacts=arguments['contacts'].value,
         role_names=arguments['role_name[]'],
         role_descriptions=arguments['role_description[]'],
-        role_prereqs=arguments['role_prereqs[]']
+        # role_prereqs=arguments['role_prereqs[]']
     ).encode('utf-8')
     return result
 
