4a8e857ed24023447eba764bf20a840339f06cfc
markchil <mark.chilenski@gmail.com>
2022-03-16 21:01:34 -0400

    Actually add perform script.

---
+++ web_scripts/performaddproject.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import cgi
+import jinja2
+
+
+def add_project(arguments):
+    import pickle as pkl
+    with open('results.pkl', 'wb') as pf:
+        pkl.dump(arguments, pf)
+
+
+def render_result_page():
+    jenv = jinja2.Environment(
+        loader=jinja2.FileSystemLoader('templates'),
+        autoescape=True
+    )
+    result = ''
+    result += 'Content-type: text/html\n\n'
+    result += '<!DOCTYPE html>\n<html lang="en">\n<head>\n<title>success?</title>\n</head>\n<body>\n<p>hello</p>\n</body>'
+    # result += jenv.get_template('projectlist.html').render().encode('utf-8')
+    return result
+
+
+def main():
+    arguments = cgi.FieldStorage()
+    add_project(arguments)
+    page = render_result_page()
+    print(page)
+
+
+if __name__ == '__main__':
+    main()
