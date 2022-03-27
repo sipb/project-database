c8f6df89611ca7dc4a33979d9dd633ce48cd26f6
markchil <mark.chilenski@gmail.com>
2022-03-26 21:35:41 -0400

    Link comm channels.

--- web_scripts/templateutils.py
+++ web_scripts/templateutils.py
@@ -1,6 +1,19 @@
 import jinja2
 from django.utils import html
 
+import strutils
+
+
+def linkify(text):
+    """Convert the given input to an HTML link of the appropriate type.
+    """
+    if text.startswith('http://') or text.startswith('https://'):
+        return '<a href="%s">%s</a>' % (text, text)
+    elif strutils.isemail(text):
+        return '<a href="mailto:%s">%s</a>' % (text, text)
+    else:
+        return text
+
 
 def get_jenv():
     """Get the jinja environment.
@@ -10,4 +23,5 @@ def get_jenv():
         autoescape=True
     )
     jenv.filters['escapejs'] = html.escapejs
+    jenv.filters['linkify'] = linkify
     return jenv
