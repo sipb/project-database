a3c1db950b90807bfca51dcae180e09bd8d0c3f6
markchil <mark.chilenski@gmail.com>
2022-05-30 14:33:59 -0400

    Add confirm dialog.

---
+++ web_scripts/confirmproject.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import cgi
+
+import authutils
+import db
+import formutils
+import strutils
+import templateutils
+import valutils
+
+# TODO: May want to turn error listing off once stable?
+import cgitb
+cgitb.enable()
+
+
+def format_confirm_project(project_id):
+    """Format the confirm project interface.
+
+    Returns
+    -------
+    result : str
+        The HTML to display.
+    """
+    jenv = templateutils.get_jenv()
+    is_valid, status_messages = valutils.validate_project_id(project_id)
+    user = authutils.get_kerberos()
+    can_edit = authutils.can_edit(user, project_id)
+    authlink = authutils.get_auth_url(True)
+    deauthlink = authutils.get_auth_url(False)
+    can_add = authutils.can_add(user)
+
+    project_info = db.get_all_info_for_project(project_id)
+    project_info = strutils.decode_utf_nested_dict_list(project_info)
+
+    result = ''
+    result += 'Content-type: text/html\n\n'
+    result += jenv.get_template('confirmproject.html').render(
+        user=user,
+        is_valid=is_valid,
+        validation_status=(
+            strutils.html_listify(status_messages) if not is_valid else ''
+        ),
+        can_edit=can_edit,
+        help_address='useful-help-email-for-projects-db [at] mit [dot] edu',
+        authlink=authlink,
+        project_info=project_info,
+        project_id=project_id,
+        deauthlink=deauthlink,
+        can_add=can_add,
+        operation='Submit'
+    ).encode('utf-8')
+    return result
+
+
+def main():
+    """Display the confirm project interface.
+    """
+    arguments = cgi.FieldStorage()
+    project_id = formutils.safe_cgi_field_get(arguments, 'project_id')
+
+    page = format_confirm_project(project_id)
+    print(page)
+
+
+if __name__ == '__main__':
+    main()
---
+++ web_scripts/performconfirmproject.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import cgi
+import traceback
+
+import authutils
+import db
+import formutils
+import mail
+import performutils
+import strutils
+import valutils
+
+# TODO: May want to turn error listing off once stable?
+import cgitb
+cgitb.enable()
+
+
+def main():
+    arguments = cgi.FieldStorage()
+    project_info = formutils.args_to_dict(arguments)
+    project_id = formutils.safe_cgi_field_get(arguments, 'project_id')
+    editor_kerberos = authutils.get_kerberos()
+    is_ok, status_messages = valutils.validate_project_id(project_id)
+    if is_ok:
+        is_ok, status_messages = valutils.validate_edit_project(
+            project_info, project_id
+        )
+
+    if is_ok:
+        try:
+            db.update_project(project_info, project_id, editor_kerberos)
+            project_info['project_id'] = project_id
+        except Exception:
+            is_ok = False
+            status = ''
+            status += 'update_project failed with the following exception:\n'
+            status += traceback.format_exc()
+            status_messages = [status]
+
+    if is_ok:
+        # When updating a rejected project, change status to
+        # "awaiting_approval" and email the approvers:
+        if db.get_project_approval_status(project_id) == 'rejected':
+            db.unreject_project(project_info, project_id, editor_kerberos)
+            mail.send_to_approvers(project_info)
+
+        page = performutils.format_success_page(project_id, 'Confirm Project')
+    else:
+        page = performutils.format_failure_page(
+            strutils.html_listify(status_messages), 'Confirm Project'
+        )
+
+    print(page)
+
+
+if __name__ == '__main__':
+    main()
