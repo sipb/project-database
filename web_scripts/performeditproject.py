62aa13a3243ae4c5eb7be54036a78cc5da8c6221
markchil <mark.chilenski@gmail.com>
2022-07-04 15:28:16 -0400

    Remove duplicated code for edit/confirm.

--- web_scripts/performeditproject.py
+++ web_scripts/performeditproject.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import cgi
-import traceback
-
-import authutils
-import db
-import formutils
-import mail
 import performutils
-import strutils
-import valutils
 
 # TODO: May want to turn error listing off once stable?
 import cgitb
@@ -18,65 +9,7 @@ cgitb.enable()
 
 
 def main():
-    arguments = cgi.FieldStorage()
-    project_info = formutils.args_to_dict(arguments)
-    project_id = formutils.safe_cgi_field_get(arguments, 'project_id')
-    editor_kerberos = authutils.get_kerberos()
-    is_ok, status_messages = valutils.validate_project_id(project_id)
-    if is_ok:
-        is_ok, status_messages = valutils.validate_edit_project(
-            project_info, project_id
-        )
-
-    is_approver = authutils.can_approve(editor_kerberos)
-    if not is_approver:
-        name_changed = performutils.check_for_name_change(
-            project_info, project_id
-        )
-        details_changed = performutils.check_for_info_change(
-            project_info, project_id
-        )
-
-    if is_ok:
-        try:
-            db.update_project(project_info, project_id, editor_kerberos)
-            project_info['project_id'] = project_id
-        except Exception:
-            is_ok = False
-            status = ''
-            status += 'update_project failed with the following exception:\n'
-            status += traceback.format_exc()
-            status_messages = [status]
-
-    if is_ok:
-        if db.get_project_approval_status(project_id) == 'rejected':
-            # When updating a rejected project, change status to
-            # "awaiting_approval" and email the approvers:
-            db.set_project_status_to_awaiting_approval(
-                project_info, project_id, editor_kerberos
-            )
-            mail.send_to_approvers(project_info)
-        elif not is_approver:
-            if name_changed:
-                # When a non-approver changes the name of a project, change
-                # status to "awaiting_approval" and email the approvers:
-                db.set_project_status_to_awaiting_approval(
-                    project_info, project_id, editor_kerberos
-                )
-                mail.send_to_approvers(project_info)
-            elif details_changed:
-                # When a non-approver changes the details of a project, send a
-                # notification email to the approvers, but do not change the
-                # project status:
-                mail.send_to_approvers(project_info)
-
-        page = performutils.format_success_page(project_id, 'Edit Project')
-    else:
-        page = performutils.format_failure_page(
-            strutils.html_listify(status_messages), 'Edit Project'
-        )
-
-    print(page)
+    performutils.edit_confirm_main('Edit')
 
 
 if __name__ == '__main__':
--- web_scripts/performutils.py
+++ web_scripts/performutils.py
@@ -1,7 +1,14 @@
+import cgi
+import traceback
+
 import authutils
 import db
+import formutils
+import mail
+import performutils
 import strutils
 import templateutils
+import valutils
 
 
 def check_for_name_change(project_info, project_id):
@@ -227,3 +234,61 @@ def format_failure_page(status, operation):
         can_add=can_add
     ).encode('utf-8')
     return result
+
+
+def edit_confirm_main(task):
+    arguments = cgi.FieldStorage()
+    project_info = formutils.args_to_dict(arguments)
+    project_id = formutils.safe_cgi_field_get(arguments, 'project_id')
+    editor_kerberos = authutils.get_kerberos()
+    is_ok, status_messages = valutils.validate_project_id(project_id)
+    if is_ok:
+        is_ok, status_messages = valutils.validate_edit_project(
+            project_info, project_id
+        )
+
+    is_approver = authutils.can_approve(editor_kerberos)
+    if not is_approver:
+        name_changed = check_for_name_change(project_info, project_id)
+        details_changed = check_for_info_change(project_info, project_id)
+
+    if is_ok:
+        try:
+            db.update_project(project_info, project_id, editor_kerberos)
+            project_info['project_id'] = project_id
+        except Exception:
+            is_ok = False
+            status = ''
+            status += 'update_project failed with the following exception:\n'
+            status += traceback.format_exc()
+            status_messages = [status]
+
+    if is_ok:
+        if db.get_project_approval_status(project_id) == 'rejected':
+            # When updating a rejected project, change status to
+            # "awaiting_approval" and email the approvers:
+            db.set_project_status_to_awaiting_approval(
+                project_info, project_id, editor_kerberos
+            )
+            mail.send_to_approvers(project_info)
+        elif not is_approver:
+            if name_changed:
+                # When a non-approver changes the name of a project, change
+                # status to "awaiting_approval" and email the approvers:
+                db.set_project_status_to_awaiting_approval(
+                    project_info, project_id, editor_kerberos
+                )
+                mail.send_to_approvers(project_info)
+            elif details_changed:
+                # When a non-approver changes the details of a project, send a
+                # notification email to the approvers, but do not change the
+                # project status:
+                mail.send_to_approvers(project_info)
+
+        page = format_success_page(project_id, '%s Project' % task)
+    else:
+        page = format_failure_page(
+            strutils.html_listify(status_messages), '%s Project' % task
+        )
+
+    print(page)
