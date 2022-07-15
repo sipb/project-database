f26a30d1f495cb5f3f97b5b85eec06d1c712676f
markchil <mark.chilenski@gmail.com>
2022-07-14 20:11:31 -0400

    Add test on get_email.

--- web_scripts/tests/test_authutils.py
+++ web_scripts/tests/test_authutils.py
@@ -13,7 +13,7 @@ assert creds.mode == 'test'
 import authutils
 
 
-class Test_get_kerberos(unittest.TestCase):
+class EmailOverrideTestCase(unittest.TestCase):
     def setUp(self):
         self.initial_email = os.getenv('SSL_CLIENT_S_DN_Email')
 
@@ -26,6 +26,8 @@ class Test_get_kerberos(unittest.TestCase):
         else:
             os.environ['SSL_CLIENT_S_DN_Email'] = self.initial_email
 
+
+class Test_get_kerberos(EmailOverrideTestCase):
     def test_none(self):
         try:
             os.environ.pop('SSL_CLIENT_S_DN_Email')
@@ -56,6 +58,39 @@ class Test_get_kerberos(unittest.TestCase):
 
         kerberos = authutils.get_kerberos()
         self.assertIsNone(kerberos)
+
+
+class Test_get_email(EmailOverrideTestCase):
+    def test_none(self):
+        try:
+            os.environ.pop('SSL_CLIENT_S_DN_Email')
+        except KeyError:
+            pass
+
+        email = authutils.get_email()
+        self.assertIsNone(email)
+
+    def test_base(self):
+        true_kerberos = 'project_test'
+        true_email = true_kerberos + '@mit.edu'
+        os.environ['SSL_CLIENT_S_DN_Email'] = true_email
+
+        email = authutils.get_email()
+        self.assertEqual(email, true_email)
+
+    def test_non_mit(self):
+        true_email = 'project_test@foo.bar'
+        os.environ['SSL_CLIENT_S_DN_Email'] = true_email
+
+        email = authutils.get_email()
+        self.assertIsNone(email)
+
+    def test_multiple_at(self):
+        true_email = 'bad@@mit.edu'
+        os.environ['SSL_CLIENT_S_DN_Email'] = true_email
+
+        email = authutils.get_email()
+        self.assertIsNone(email)
 
 
 if __name__ == '__main__':
