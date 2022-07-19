81d7bc95666cc0009c824e03a739266fd5d350f5
markchil <mark.chilenski@gmail.com>
2022-07-18 20:07:20 -0400

    Validate project_comm_channels.

--- web_scripts/tests/test_valutils.py
+++ web_scripts/tests/test_valutils.py
@@ -592,5 +592,44 @@ class Test_validate_project_links(unittest.TestCase):
         self.assertGreaterEqual(len(status_messages), 1)
 
 
+class Test_validate_project_comm_channels(unittest.TestCase):
+    def test_empty(self):
+        is_ok, status_messages = valutils.validate_project_comm_channels([])
+        self.assertTrue(is_ok)
+        self.assertEqual(len(status_messages), 0)
+
+    def test_valid(self):
+        is_ok, status_messages = valutils.validate_project_comm_channels(
+            [
+                {
+                    'commchannel': 'mattermost',
+                    'index': 0
+                },
+                {
+                    'commchannel': 'carrier pigeon',
+                    'index': 1
+                }
+            ]
+        )
+        self.assertTrue(is_ok)
+        self.assertEqual(len(status_messages), 0)
+
+    def test_invalid(self):
+        is_ok, status_messages = valutils.validate_project_comm_channels(
+            [
+                {
+                    'commchannel': 'mattermost',
+                    'index': 0
+                },
+                {
+                    'commchannel': 'mattermost',
+                    'index': 1
+                }
+            ]
+        )
+        self.assertFalse(is_ok)
+        self.assertGreaterEqual(len(status_messages), 1)
+
+
 if __name__ == '__main__':
     unittest.main()
