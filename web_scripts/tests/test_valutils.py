32cd6dae5560b2740893db294eaa4b60e12d0120
markchil <mark.chilenski@gmail.com>
2022-07-22 20:09:37 -0400

    Test validate_approval_comments.

--- web_scripts/tests/test_valutils.py
+++ web_scripts/tests/test_valutils.py
@@ -1059,5 +1059,28 @@ class Test_validate_approval_action(unittest.TestCase):
         self.assertGreaterEqual(len(status_messages), 1)
 
 
+class Test_validate_approval_comments(unittest.TestCase):
+    def test_approved(self):
+        is_ok, status_messages = valutils.validate_approval_comments(
+            'accepted', ''
+        )
+        self.assertTrue(is_ok)
+        self.assertEqual(len(status_messages), 0)
+
+    def test_rejected_valid(self):
+        is_ok, status_messages = valutils.validate_approval_comments(
+            'rejected', 'this is a comment'
+        )
+        self.assertTrue(is_ok)
+        self.assertEqual(len(status_messages), 0)
+
+    def test_rejected_invalid(self):
+        is_ok, status_messages = valutils.validate_approval_comments(
+            'rejected', ''
+        )
+        self.assertFalse(is_ok)
+        self.assertGreaterEqual(len(status_messages), 1)
+
+
 if __name__ == '__main__':
     unittest.main()
