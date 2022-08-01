9ffeef220f9e4f58d35da8a373d40cf5c61b1596
markchil <mark.chilenski@gmail.com>
2022-08-01 19:30:29 -0400

    Set paths in correct blocks.

--- push.sh
+++ push.sh
@@ -5,9 +5,9 @@ cmd_path=$(dirname -- "$( readlink -f -- "$0"; )")
 
 if [[ cmd_path == "/afs/sipb.mit.edu/project/sipb-projectdb"* ]]
 then
-    web_scripts_path=/mit/$(whoami)/web_scripts/
-else
     web_scripts_path=/mit/sipb-projectdb/web_scripts/
+else
+    web_scripts_path=/mit/$(whoami)/web_scripts/
 fi
 
 echo $cmd_path
@@ -16,5 +16,6 @@ echo $web_scripts_path
 
 cd "$(dirname "$0")"
 # rsync -av web_scripts/ $web_scripts_path
+
 #chmod 0777 /mit/hwops/web_scripts/robots.txt
 #rsync -av manuals/ /mit/hwops/web_scripts/manuals/
