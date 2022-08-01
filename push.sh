10d393d80e39ae57a6b206dc34e72de894283294
markchil <mark.chilenski@gmail.com>
2022-08-01 19:25:34 -0400

    Testing in push script.

--- push.sh
+++ push.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e -u
+
+cmd_path=$(dirname -- "$( readlink -f -- "$0"; )")
+
+if [[ cmd_path == *"sipb-projectdb"* ]]
+then
+    web_scripts_path=/mit/$(whoami)/web_scripts/
+else
+    web_scripts_path=/mit/sipb-projectdb/web_scripts/
+fi
+
+echo $web_scripts_path
+
+
 cd "$(dirname "$0")"
-rsync -av web_scripts/ /mit/$(whoami)/web_scripts/
+# rsync -av web_scripts/ $web_scripts_path
 #chmod 0777 /mit/hwops/web_scripts/robots.txt
 #rsync -av manuals/ /mit/hwops/web_scripts/manuals/
