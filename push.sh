03e3f8572f998fe7b791fb88f20cfa5e87775c7f
markchil <mark.chilenski@gmail.com>
2022-08-01 19:57:29 -0400

    Restore push command.

--- push.sh
+++ push.sh
@@ -12,12 +12,8 @@ else
     web_scripts_path=/mit/$(whoami)/web_scripts/
 fi
 
-echo $cmd_path
-echo $web_scripts_path
-
-
 cd "$(dirname "$0")"
-# rsync -av web_scripts/ $web_scripts_path
+rsync -av web_scripts/ $web_scripts_path
 
 #chmod 0777 /mit/hwops/web_scripts/robots.txt
 #rsync -av manuals/ /mit/hwops/web_scripts/manuals/
