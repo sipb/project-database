6bbc9649adc0f9524385d65e3a7521d8b0b602f7
Huy Dai <huydai@mit.edu>
2022-08-14 00:09:55 -0500

    Try to troubleshoot error with FAQ

--- web_scripts/faq.py
+++ web_scripts/faq.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import cgi
+
 import authutils
+import db
+import formutils
+import strutils
 import templateutils
+
 
 # TODO: May want to turn error listing off once stable?
 import cgitb
@@ -18,17 +24,21 @@ def format_faq():
     """
     jenv = templateutils.get_jenv()
     user = authutils.get_kerberos()
+    user_email = authutils.get_email()
     authlink = authutils.get_auth_url(True)
     deauthlink = authutils.get_auth_url(False)
     can_add = authutils.can_add(user)
+    can_approve = authutils.can_approve(user)
 
     result = ''
     result += 'Content-type: text/html\n\n'
     result += jenv.get_template('faq.html').render(
         user=user,
+        user_email=user_email,
         authlink=authlink,
         deauthlink=deauthlink,
         can_add=can_add,
+        can_approve=can_approve
     ).encode('utf-8')
     return result
 
