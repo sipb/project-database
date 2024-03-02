4ae82a1f23c8b2bb4d6d76ee756ad106f4b78f81
Huy Dai <huydai@mit.edu>
2024-03-02 13:16:47 -0500

    Update admin users to reflect new SIPB EC

--- web_scripts/config.py
+++ web_scripts/config.py
@@ -1,3 +1,3 @@
 EXPIRATION_BY_NUM_DAYS = 365
-ADMIN_USERS = ['huydai', 'javsolis', 'markchil']
+ADMIN_USERS = ['huydai', 'markchil', 'innaavo', 'psvenk', 'alwinfy', 'aabreu', 'arjunjb', 'amigdal', 'turino14', 'nmorgan']
 APPROVER_USERS = []
