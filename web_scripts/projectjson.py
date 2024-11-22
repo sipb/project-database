10558672f33b977e0144589c2960da8d1c3457fc
Gabriel Rodríguez <rgabriel@mit.edu>
2024-11-22 10:27:00 -0500

    work around datetime not json serializable

--- web_scripts/projectjson.py
+++ web_scripts/projectjson.py
@@ -15,7 +15,9 @@ def main():
     
     jenv = templateutils.get_jenv()
     all_projects_list = db.list_dict_convert(db.get_all_project_info('approved') ,remove_sql_ref=True)
-    all_projects_json = json.dumps({"projects": all_projects_list})
+    # https://stackoverflow.com/a/36142844/5031798, converts datetimes to str although
+    # not in a very machine parseable way
+    all_projects_json = json.dumps({"projects": all_projects_list}, default=str)
     
     result = ''
     result += 'Content-type: application/json\n\n'
